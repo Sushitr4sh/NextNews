49003229eb1aefe6c1c98fc5e38172e8d21beb84
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2024-10-09 16:03:51 +0700

    Add app styling & using dummy data

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
 import React from "react";
 
+import { DUMMY_NEWS } from "../../dummy-news";
+
 const News = () => {
   return (
     <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/111">First News Item</Link>
-        </li>
-        <li>
-          <Link href="/news/222">Second News Item</Link>
-        </li>
-        <li>
-          <Link href="/news/333">Third News Item</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -3,16 +3,18 @@ import React from "react";
 
 const MainHeader = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/news">News</Link>
-        </li>
-      </ul>
-    </nav>
+    <header id="main-header">
+      <div id="logo">
+        <Link href="/">NextNews</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/news">News</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
