9e807e87739d6d7db67671038e5d5817f2e1e7e6
Mario Imanuel Daruranto <mariodaruranto68@gmail.com>
2024-10-11 11:04:19 +0700

    Add fallback content for not found page

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DUMMY_NEWS } from "../../../dummy-news";
+import { notFound } from "next/navigation";
 
 type NewsDetailProps = {
   params: { slug: number | string };
@@ -8,6 +9,9 @@ type NewsDetailProps = {
 const NewsDetail = ({ params }: NewsDetailProps) => {
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+  if (!newsItem) {
+    notFound(); // Show closest not found fallback content
+  }
   return (
     <article className="news-article">
       <header>
